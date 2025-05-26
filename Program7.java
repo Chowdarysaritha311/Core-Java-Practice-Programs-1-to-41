50d09a45c3244906a22f47efed785bb5a7ab8d0e
CHOWDARY SARITHA <142637859+Chowdarysaritha311@users.noreply.github.com>
2025-05-26 22:59:58 +0530

    Add files via upload

---
+++ Program7.java
@@ -0,0 +1,8 @@
+public class Program7 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+    }
+}
---
+++ Program9.java
@@ -0,0 +1,7 @@
+public class Program9 {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        int max = (a > b) ? a : b;
+        System.out.println("Max: " + max);
+    }
+}
